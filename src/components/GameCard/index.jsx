5ed482f3df0907e7c50a16b5ddd94a59f876ac21
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-10-28 11:49:09 +0200

    Create the gamecards css and settled the basic form of the cards. Still have details like colors or font, and to adapt size depending the situation to do

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,49 +1,52 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
-const GameCard = ({ heroApi }) => {
+const GameCard = (props) => {
   const [basicInfo, setBasicInfo] = useState([]);
   const [stats, setStats] = useState([]);
   const [visual, setVisual] = useState([]);
 
   useEffect(() => {
     axios
-      .get({ heroApi })
+      .get(props.heroApi)
       .then((resp) => resp.data)
       .then((data) => setBasicInfo(data));
   }, []);
 
   useEffect(() => {
     axios
-      .get({ heroApi })
+      .get(props.heroApi)
       .then((resp) => resp.data)
       .then((data) => setStats(data.powerstats));
   }, []);
 
   useEffect(() => {
     axios
-      .get({ heroApi })
+      .get(props.heroApi)
       .then((resp) => resp.data)
       .then((data) => setVisual(data.image));
   }, []);
 
   return (
     <article className="gameCard">
-      <section>
+      <section className="cardVisual">
         <img src={visual.url} alt="" />
       </section>
-      <section>
-        <p>
-          {(parseInt(stats.power, 10) +
-            parseInt(stats.speed, 10) +
-            parseInt(stats.intelligence, 10)) /
-            3}
+      <section className="playValue">
+        <p className="battleStat">
+          {Math.round(
+            (parseInt(stats.power, 10) +
+              parseInt(stats.speed, 10) +
+              parseInt(stats.intelligence, 10)) /
+              3
+          )}
         </p>
-        <p>Rarity</p>
-        <p>{parseInt(stats.combat, 10)}</p>
+        <p className="rarityStat">Rarity</p>
+        <p className="summonStat">{parseInt(stats.combat, 10)}</p>
       </section>
-      <section>
+      <section className="charaName">
         <p>{basicInfo.name}</p>
       </section>
     </article>
