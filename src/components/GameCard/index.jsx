faff592c44e37d0036288ddc4214d605b2562829
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-10-29 17:21:47 +0200

    Made a star pop in the card to indicate the rarity level of card

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,6 +1,11 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import star1 from './cardsIcons/etoile1.svg';
+import star2 from './cardsIcons/etoile2.svg';
+import star3 from './cardsIcons/etoile3.svg';
+import star4 from './cardsIcons/etoile4.svg';
+import star5 from './cardsIcons/etoile5.svg';
 import './index.css';
 
 const GameCard = ({ heroApi, cardSize }) => {
@@ -8,6 +13,7 @@ const GameCard = ({ heroApi, cardSize }) => {
   const [stats, setStats] = useState([]);
   const [visual, setVisual] = useState([]);
   const [borderColor, setBorderColor] = useState('');
+  const [rarity, setRarity] = useState('');
 
   useEffect(() => {
     axios
@@ -43,14 +49,19 @@ const GameCard = ({ heroApi, cardSize }) => {
   useEffect(() => {
     if (rarityLevel >= 80) {
       setBorderColor('legendaryCard');
+      setRarity(star5);
     } else if (rarityLevel >= 60 && rarityLevel < 80) {
       setBorderColor('epicCard');
+      setRarity(star4);
     } else if (rarityLevel >= 40 && rarityLevel < 60) {
       setBorderColor('ultraCard');
+      setRarity(star3);
     } else if (rarityLevel >= 20 && rarityLevel < 40) {
       setBorderColor('rareCard');
+      setRarity(star2);
     } else {
       setBorderColor('commonCard');
+      setRarity(star1);
     }
   });
 
@@ -68,7 +79,9 @@ const GameCard = ({ heroApi, cardSize }) => {
               3
           )}
         </p>
-        <p className="rarityStat">{rarityLevel}</p>
+        <div className="rarityStat">
+          <img className="rarityStar" src={rarity} alt="" />
+        </div>
         <p className="summonStat">
           {Math.round(
             (parseInt(stats.combat, 10) +
