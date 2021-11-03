051ffe87e64536f10805bce6e6459ee0cf883915
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2021-11-03 17:14:14 +0100

    Update et delete elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import HomeBlog from './Components/HomeBlog';
-import wildHeroes from './Images/wildHeroes.png';
-import './App.css';
-
 function App() {
-  return (
-    <div>
-      <img className="logo" src={wildHeroes} alt="wildHeroes" />
-      <HomeBlog />
-    </div>
-  );
+  return <body />;
 }
 
 export default App;
