c7726edb90fcaf549ee276234aa2a5892022c109
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2021-11-03 15:39:18 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import HomeBlog from './Components/HomeBlog';
-import WildHeroes from './Images/WildHeroes.png';
+import wildHeroes from './Images/wildHeroes.png';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <img className="logo" src={WildHeroes} alt="WildHeroes" />
+      <img className="logo" src={wildHeroes} alt="wildHeroes" />
       <HomeBlog />
     </div>
   );
