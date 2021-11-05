f35dd993a04ebba9d4d1818bac41462781ded79f
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2021-11-05 12:11:25 +0100

    modification of css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import './Components/slider/Slider.css';
+import ImageSlider from './Components/slider/ImageSlider';
+import SliderData from './Components/slider/SliderData';
+
 function App() {
-  return <div />;
+  return <ImageSlider slides={SliderData} />;
 }
+
 export default App;
