ab41ab67ac3baed6f684cef5a575fff594b6190e
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2021-11-15 12:04:16 +0100

    update home with home blog

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './index.css';
+import wildHeroes from '../../Images/wildHeroes.png';
+import HomeBlog from '../HomeBlog';
 
 function Home() {
   return (
     <div className="home">
-      <p>Home</p>
+      <img className="logo" src={wildHeroes} alt="wildHeroes" />
+      <HomeBlog />
     </div>
   );
 }
