4357629c37c344458105ee058ddeeaa50a0b2794
Romain Brehon <romain.brehon@yahoo.fr>
2021-11-15 21:44:01 +0100

    finished the alternate color effet on mouseover YAY! code can be prettier?

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -8,12 +8,36 @@ import BlackCollection from './Logo/BlackCollection.png';
 import BlackRules from './Logo/BlackRules.png';
 import BlackNews from './Logo/BlackNews.png';
 import YellowBars from './Logo/YellowBars.png';
+import YellowHome from './Logo/YellowHome.png';
+import YellowGame from './Logo/YellowGame.png';
+import YellowCollection from './Logo/YellowCollection.png';
+import YellowRules from './Logo/YellowRules.png';
+import YellowNews from './Logo/YellowNews.png';
 import './index.css';
 
 function NavBar() {
   const [sidebar, setSidebar] = useState(false);
-
   const showSidebar = () => setSidebar(!sidebar);
+
+  // const [yellowIcons, setYellowIcons] = useState(false);
+  // const showYellowIcons = () => setYellowIcons(true);
+  // const hideYellowIcons = () => setYellowIcons(false);
+
+  const [yellowHome, setYellowHome] = useState(false);
+  const showYellowHome = () => setYellowHome(true);
+  const hideYellowHome = () => setYellowHome(false);
+  const [yellowGame, setYellowGame] = useState(false);
+  const showYellowGame = () => setYellowGame(true);
+  const hideYellowGame = () => setYellowGame(false);
+  const [yellowCollection, setYellowCollection] = useState(false);
+  const showYellowCollection = () => setYellowCollection(true);
+  const hideYellowCollection = () => setYellowCollection(false);
+  const [yellowRules, setYellowRules] = useState(false);
+  const showYellowRules = () => setYellowRules(true);
+  const hideYellowRules = () => setYellowRules(false);
+  const [yellowNews, setYellowNews] = useState(false);
+  const showYellowNews = () => setYellowNews(true);
+  const hideYellowNews = () => setYellowNews(false);
 
   return (
     <>
@@ -27,24 +51,52 @@ function NavBar() {
         <div className="burgerImg">
           <img src={WildHeroesBurger} alt="Wild Heroes" />
         </div>
-        <Link to="/" className="menuBars">
-          <img src={BlackHome} alt="Home" />
+        <Link
+          to="/"
+          className="menuBars"
+          onMouseEnter={showYellowHome}
+          onMouseLeave={hideYellowHome}
+        >
+          <img src={yellowHome ? YellowHome : BlackHome} alt="Home" />
           <p className={sidebar ? 'textShow' : 'textHide'}>Home</p>
         </Link>
-        <Link to="/newgame" className="menuBars">
-          <img src={BlackGame} alt="Game" />
+        <Link
+          to="/newgame"
+          className="menuBars"
+          onMouseEnter={showYellowGame}
+          onMouseLeave={hideYellowGame}
+        >
+          <img src={yellowGame ? YellowGame : BlackGame} alt="Game" />
           <p className={sidebar ? 'textShow' : 'textHide'}>Play Game</p>
         </Link>
-        <Link to="/collection" className="menuBars">
-          <img src={BlackCollection} alt="Collection" />
+        <Link
+          to="/collection"
+          className="menuBars"
+          onMouseEnter={showYellowCollection}
+          onMouseLeave={hideYellowCollection}
+        >
+          <img
+            src={yellowCollection ? YellowCollection : BlackCollection}
+            alt="Collection"
+          />
           <p className={sidebar ? 'textShow' : 'textHide'}>Collection</p>
         </Link>
-        <Link to="/rules" className="menuBars">
-          <img src={BlackRules} alt="Rules" />
+        <Link
+          to="/rules"
+          className="menuBars"
+          onMouseEnter={showYellowRules}
+          onMouseLeave={hideYellowRules}
+        >
+          <img src={yellowRules ? YellowRules : BlackRules} alt="Rules" />
           <p className={sidebar ? 'textShow' : 'textHide'}>Rules</p>
         </Link>
-        <Link to="/news" className="menuBars">
-          <img src={BlackNews} alt="News" />
+        <Link
+          to="/news"
+          className="menuBars"
+          onMouseEnter={showYellowNews}
+          onMouseLeave={hideYellowNews}
+        >
+          <img src={yellowNews ? YellowNews : BlackNews} alt="News" />
           <p className={sidebar ? 'textShow' : 'textHide'}>News</p>
         </Link>
       </nav>
