d6d5c22df6d75cb792fd075d980dfc4a912389e5
Jérémy Vandomme <83273965+Vjerem@users.noreply.github.com>
2021-11-15 09:38:23 +0100

    add caroussel update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import HomeCarousel from './Components/Carousel';
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <HomeCarousel />
+    </div>
+  );
 }
 
 export default App;
