ac297b675d45910d846268394b537047cd0febf2
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-11-17 09:55:36 +0100

    Deleted the logo directly load in Home to change it into a component

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import HomeBlog from '../HomeBlog';
 import HomeCarousel from '../Carousel';
 import './index.css';
-import wildHeroes from '../Images/TopWildHeroes.png';
 
 function Home() {
   return (
     <div className="home">
-      <img className="logo" src={wildHeroes} alt="wildHeroes" />
       <HomeBlog />
       <HomeCarousel />
     </div>
