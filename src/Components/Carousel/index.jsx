7bd613f7e88cf1a4627f7572b9b517f6c1c5b134
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-11-17 11:53:16 +0100

    Fixed the img problem by replacing the image tag in the Carousel css by a class

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -4,22 +4,27 @@ import './index.css';
 const SlideCarousel = () => {
   const slides = [
     <img
+      className="carouselImg"
       src="https://www.superherodb.com/pictures2/portraits/10/100/659.jpg"
       alt="1"
     />,
     <img
+      className="carouselImg"
       src="https://www.superherodb.com/pictures2/portraits/10/100/248.jpg"
       alt="2"
     />,
     <img
+      className="carouselImg"
       src="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
       alt="3"
     />,
     <img
+      className="carouselImg"
       src="https://www.superherodb.com/pictures2/portraits/10/100/247.jpg"
       alt="4"
     />,
     <img
+      className="carouselImg"
       src="https://www.superherodb.com/pictures2/portraits/10/100/85.jpg"
       alt="5"
     />,
