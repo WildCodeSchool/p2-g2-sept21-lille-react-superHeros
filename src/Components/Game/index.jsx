8e8c872503413a5a2f90db952b78df63abac5327
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-11-18 10:51:53 +0100

    Made a Work in Progress window

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import WildLogo from '../WildLogo';
+import CaptainHammer from './Gif/captain-hammer.gif';
+import './index.css';
 
 function Game() {
   return (
-    <div>
-      <p>Game</p>
+    <div className="gameLayout">
+      <WildLogo />
+      <div className="wop">
+        <div className="wopGif">
+          <img src={CaptainHammer} alt="Captain America with Thor's hammer" />
+        </div>
+        <h3 className="wopTitle">Work In Progress !</h3>
+        <h4 className="wopSub">
+          But first, we have to take the hammer back...
+        </h4>
+      </div>
     </div>
   );
 }
